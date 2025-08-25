9458abd0bfc6d1b83cc961d49ab74a559567cc2f
Apeksha Verma <apekshaverma@Apekshas-MacBook-Air.local>
2025-08-25 13:51:21 +0530

    build done

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-const parent = React.createElement("div",{id: "parent"},
-[    React.createElement("div",{id: "child1"},
-        React.createElement("h1",{},"I am an h1 tag"),
-          React.createElement("h2",{},"I am an h2 tag"),
-),    React.createElement("div",{id: "child2"},
-       [ React.createElement("h1",{},"I am an h1 tag"),
-          React.createElement("h2",{},"I am an h2 tag"),]
-)]
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement(
+    "div",
+    { id: "child1", key: "child1" },
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag")
+  ),
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement("h1", {key: "h1"}, "I am an h1 tag"),
+    React.createElement("h2", {key: "h2"}, "I am an h2 tag"),
+  ]),
+]);
 
 // const heading = React.createElement(
 //   "h1",
 //   { id: "heading"},
 //   "Hello World from React"
 // );
-console.log(parent)
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
