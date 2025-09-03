b72c2a85ce3ad828704ccad0cb36322a72002547
Apeksha Verma <apekshaverma@Apekshas-MacBook-Air.local>
2025-09-03 15:38:17 +0530

    custom hook added

---
+++ src/components/User.js
@@ -0,0 +1,9 @@
+const User =({name})=>{
+    return <div className="user-card">
+<h2>Name: {name}</h2>
+<h3>Location: Greater Noida</h3>
+<h4>Contact: @itsapeksha16</h4>
+    </div>
+}
+
+export default User;
